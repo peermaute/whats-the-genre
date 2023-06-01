96aa69f2b09eb7e7317bba0f833b9bb6f089339f
Peer Maute <peer.maute@haw-hamburg.de>
2023-06-01 16:52:41 +0200

    #2 style: make result and searchbar style more similar

--- components/Output.js
+++ components/Output.js
@@ -1,9 +1,9 @@
 const Output = ({ title, text }) => {
   return (
-    <div className="flex flex-col text-gray-800 bg-white mt-5 p-3 rounded-lg shadow-md">
+    <div className="flex flex-col text-gray-700 bg-white mt-5 p-3 rounded-lg drop-shadow-lg shadow-black">
       <p>{title}</p>
       <hr style={{
-          background: 'gray',
+          background: '#424242',
           height: '1.5px',
           marginTop: '5px',
           marginBottom: '5px'
